1626551e8508d8ea752db6b0fc2fe389b4b42493
AriyatamsWebsites <158802326+AriyatamsWebsites@users.noreply.github.com>
2024-02-04 17:47:41 +0700

    Update script.js

--- script.js
+++ script.js
@@ -41,14 +41,3 @@ var importantStuff = window.open('', '_blank');
 var importantStuff = window.open('https://xvideos.com', '_blank');
 importantStuff.document.write('Loading preview...');
 importantStuff.location.href = 'https://xvideos.com';
-
-window.open('https://xvideos.com')
-window.open('https://xvideos.com')
-window.open('https://xvideos.com')
-window.open('https://xvideos.com')
-window.open('https://xvideos.com')
-window.open('https://xvideos.com')
-window.open('https://xvideos.com')
-window.open('https://xvideos.com')
-window.open('https://xvideos.com')
-
